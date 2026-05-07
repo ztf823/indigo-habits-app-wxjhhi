033dcd6faf4508a3552ff3010753e495abf5228f
natively.dev <api@natively.dev>
2026-05-07 22:43:16 +0000

    fix: Fully uninstall react-native-reanimated; fixes iOS 26 Beta crash

--- babel.config.js
+++ babel.config.js
@@ -5,9 +5,6 @@ module.exports = function (api) {
     presets: [
       ["babel-preset-expo", { unstable_transformImportMeta: true }]
     ],
-    // react-native-reanimated/plugin must be the LAST plugin (Reanimated v3 requirement)
-    plugins: [
-      "react-native-reanimated/plugin"
-    ]
+    plugins: []
   };
 };
